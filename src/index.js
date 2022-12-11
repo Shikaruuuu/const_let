353c6a78cff6efca47dd0225dd5dd62433bd6d7a
Shikaruuuu <40463368+Shikaruuuu@users.noreply.github.com>
2022-12-11 16:24:13 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/**
+ * const, let
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// //変数varは上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再定義";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "letは上書き可能";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// //constは上書き不可
+// val3 = "const変数を上書き";
+
+const val3 = "const変数再宣言";
